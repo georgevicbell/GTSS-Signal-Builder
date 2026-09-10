507ff7faf57d27c9428c010c9a07feb210e50152
George Bell <4380159+georgevicbell@users.noreply.github.com>
2026-09-10 04:04:11 -0400

    eslint more

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+export default defineConfig([
+  {
+    ignores: ["**/dist/**"],
+
+    extends: [js.configs.recommended, tseslint.configs.recommended],
+  },
+]);
