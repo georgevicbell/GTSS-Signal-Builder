3781c2c9ca5605d61bc8278141715bd67fd6abc8
George Bell <4380159+georgevicbell@users.noreply.github.com>
2026-09-11 01:41:30 -0400

    update for ts/js

--- eslint.config.js
+++ eslint.config.js
@@ -2,9 +2,15 @@ import js from "@eslint/js";
 import { defineConfig } from "eslint/config";
 import tseslint from "typescript-eslint";
 export default defineConfig([
+  tseslint.configs.recommended,
+  js.configs.recommended,
   {
     ignores: ["**/dist/**"],
-
+    settings: {
+      react: {
+        version: "19.2.3", // Match this with your project's React version in package.json
+      },
+    },
     extends: [js.configs.recommended, tseslint.configs.recommended],
   },
 ]);
