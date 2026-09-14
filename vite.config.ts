0343419af3ab17ca0127ded268619b92e51d8085
George Bell <4380159+georgevicbell@users.noreply.github.com>
2026-09-14 19:16:51 -0400

    Get tests running on github (#44)
    * try testing on github

--- vite.config.ts
+++ vite.config.ts
@@ -30,4 +30,12 @@ export default defineConfig({
       deny: ["**/.*"],
     },
   },
+   test: {
+    coverage: {
+      // you can include other reporters, but 'json-summary' is required, json is recommended
+      reporter: ['text', 'json-summary', 'json'],
+      // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
+      reportOnFailure: true,
+    }
+  }
 });
